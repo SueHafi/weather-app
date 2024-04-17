56e46b059775890a686d4c9dbdb035405f85a2f3
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-04-17 16:57:11 +0100

    fixed endpoint protocol to be https

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -28,21 +28,21 @@ export async function weatherApi(locationText) {
 
 async function locationApiCall(locationText) {
   const response = await fetch(
-    `http://dataservice.accuweather.com/locations/v1/search?apikey=${apiKey}&q=${locationText}`
+    `https://dataservice.accuweather.com/locations/v1/search?apikey=${apiKey}&q=${locationText}`
   );
   return await response.json();
 }
 
 async function currentConditionApiCall(locationKey) {
   const response = await fetch(
-    `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
+    `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
   );
   return await response.json();
 }
 
 async function forecast(locationKey) {
   const response = await fetch(
-    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}`
+    `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}`
   );
   return await response.json();
 }
