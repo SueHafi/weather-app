b6235b6537bf80729fefc2038ad2e8f1be6d181f
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-06-15 14:27:32 +0100

    added cypress end to end testing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,57 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.intercept('GET', 'https://dataservice.accuweather.com/*', null);
-    cy.visit('http://localhost:5173/');
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://dataservice.accuweather.com/locations/**",
+      },
+      {
+        statusCode: 503,
+      }
+    );
+    cy.visit("/");
+    cy.findByText("Milton Keynes");
+  });
+
+  it("loads data after user search", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://dataservice.accuweather.com/locations/**",
+      },
+      {
+        statusCode: 503,
+      }
+    );
+    cy.visit("/");
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://dataservice.accuweather.com/locations/**",
+      },
+      {
+        fixture: "locationData.json",
+      }
+    );
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://dataservice.accuweather.com/currentconditions/**",
+      },
+      {
+        fixture: "currentConditionData.json",
+      }
+    );
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://dataservice.accuweather.com/forecasts/**",
+      },
+      {
+        fixture: "forecastData.json",
+      }
+    );
+    cy.findByRole('searchbox').type('Leeds{enter}');
+    cy.findByText('Leeds');
+  });
+});
